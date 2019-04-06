79c632994cee5bfedfcaf874503eaace87003c5a
Jonathan Cruz <jitzian@gmail.com>
2019-04-05 23:53:23 -0400

    ks jc - Just adding baseURL

--- app/src/main/java/org/com/raian/krasamocodechallenge/constants/GlobalConstants.kt
+++ app/src/main/java/org/com/raian/krasamocodechallenge/constants/GlobalConstants.kt
@@ -2,6 +2,6 @@ package org.com.raian.krasamocodechallenge.constants
 
 class GlobalConstants {
     companion object {
-        const val baseUrl = ""
+        const val baseUrl = "https://api.iextrading.com/1.0/"
     }
 }
