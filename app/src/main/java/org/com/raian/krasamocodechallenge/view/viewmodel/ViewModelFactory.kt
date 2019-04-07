024123c55c7ecbe80d7f43efa01f16c62e92c999
Jonathan Cruz <jitzian@gmail.com>
2019-04-07 02:58:37 -0400

    ks jc - Just closing access to context parameter to avoid leaks

--- app/src/main/java/org/com/raian/krasamocodechallenge/view/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/org/com/raian/krasamocodechallenge/view/viewmodel/ViewModelFactory.kt
@@ -5,7 +5,7 @@ import android.arch.lifecycle.ViewModelProvider
 import android.content.Context
 
 @Suppress("UNCHECKED_CAST")
-class ViewModelFactory(val context: Context) : ViewModelProvider.Factory {
+class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(StockViewModel::class.java)){
             return StockViewModel(context) as T
