082058cceca0a3f6ff9e04108b4b73534db1da76
Jonathan Cruz <jitzian@gmail.com>
2019-04-07 16:22:33 -0400

    ks jc - Cleanup. Removing context due to it is not used.

--- app/src/main/java/org/com/raian/krasamocodechallenge/view/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/org/com/raian/krasamocodechallenge/view/viewmodel/ViewModelFactory.kt
@@ -5,10 +5,10 @@ import android.arch.lifecycle.ViewModelProvider
 import android.content.Context
 
 @Suppress("UNCHECKED_CAST")
-class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+class ViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(StockViewModel::class.java)){
-            return StockViewModel(context) as T
+        if (modelClass.isAssignableFrom(StockViewModel::class.java)) {
+            return StockViewModel() as T
         }
         throw IllegalArgumentException("ViewModel class is not defined")
     }
