cbf027fe9814c729dcc3cf0159caeafd8ab346f2
Jonathan Cruz <jitzian@gmail.com>
2019-04-08 15:44:29 -0400

    ks jc - Wiring BaseActivity to react whenever connectivity is lost.

--- app/src/main/java/org/com/raian/krasamocodechallenge/view/activities/BaseActivity.kt
+++ app/src/main/java/org/com/raian/krasamocodechallenge/view/activities/BaseActivity.kt
@@ -1,17 +1,55 @@
 package org.com.raian.krasamocodechallenge.view.activities
 
 import android.arch.lifecycle.ViewModelProviders
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import org.com.raian.krasamocodechallenge.R
+import org.com.raian.krasamocodechallenge.util.NetworkReceiver
 import org.com.raian.krasamocodechallenge.view.viewmodel.StockViewModel
 import org.com.raian.krasamocodechallenge.view.viewmodel.ViewModelFactory
 import java.util.logging.Logger
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity(), NetworkReceiver.NetworkListener {
     lateinit var TAG: String
     lateinit var logger: Logger
+    private lateinit var mSnackBar: Snackbar
+    private lateinit var networkReceiver: NetworkReceiver
     internal val viewModel by lazy {
-        ViewModelProviders.of(this, ViewModelFactory(this)).get(StockViewModel::class.java)
+        ViewModelProviders.of(this, ViewModelFactory()).get(StockViewModel::class.java)
+    }
+
+    override fun onStart() {
+        super.onStart().also {
+            networkReceiver = NetworkReceiver(this)
+            registerReceiver(networkReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+        }
     }
 
     abstract fun initViews()
+
+    override fun onStop() {
+        super.onStop().also {
+            unregisterReceiver(networkReceiver)
+        }
+    }
+
+    override fun getNetworkStatus(isConnected: Boolean) {
+        showMessage(isConnected)
+    }
+
+    private fun showMessage(isConnected: Boolean) {
+        if (!isConnected) {
+            mSnackBar = Snackbar.make(
+                findViewById(R.id.mLayoutMainActivityContainer),
+                "There is no Connectivity",
+                Snackbar.LENGTH_LONG
+            ) //Assume "rootLayout" as the root layout of every activity.
+            mSnackBar.duration = Snackbar.LENGTH_LONG
+            mSnackBar.show()
+        }
+
+    }
+
 }
